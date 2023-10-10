c60dea18a1b9384774a397e37f892cd5758cdfeb
Simon Lynch <simon.lynch@hashicorp.com>
2023-10-10 21:14:56 +1100

    add for_each

--- main.tf
+++ main.tf
@@ -2,3 +2,41 @@ resource "tfe_project" "this" {
   name         = var.project_name
   organization = var.organization_name
 }
+
+resource "tfe_team" "this" {
+  name = var.team_name
+}
+
+resource "tfe_team_project_access" "default" {
+  for_each = var.team_project_access
+
+  access     = each.value
+  team_id    = tfe_team.this.id
+  project_id = tfe_project.this.id
+}
+
+resource "tfe_team_project_access" "custom" {
+  for_each = var.custom_team_project_access
+
+  access     = "custom"
+  team_id    = tfe_team.this.id
+  project_id = tfe_project.this.id
+
+  project_access {
+    settings = try(each.value.project_access.settings, "read")
+    teams    = try(each.value.project_access.teams, "none")
+  }
+
+  workspace_access {
+    runs           = try(each.value.workspace_access.runs, "read")
+    sentinel_mocks = try(each.value.workspace_access.sentinel_mocks, "none")
+    state_versions = try(each.value.workspace_access.state_versions, "none")
+
+    variables = try(each.value.workspace_access.variables, "none")
+    create    = try(each.value.workspace_access.create, false)
+    locking   = try(each.value.workspace_access.locking, false)
+    delete    = try(each.value.workspace_access.delete, false)
+    move      = try(each.value.workspace_access.move, false)
+    run_tasks = try(each.value.workspace_access.run_tasks, false)
+  }
+}
--- variables.tf
+++ variables.tf
@@ -7,3 +7,35 @@ variable "project_name" {
 variable "organization_name" {
   type = string
 }
+variable "team_name" {
+  type = string
+}
+
+variable "team_project_access" {
+  type        = map(string)
+  description = "Map of existing Team(s) and built-in permissions to grant on Workspace."
+  default     = {}
+}
+
+variable "custom_team_project_access" {
+  type = map(
+    object({
+      project_access = object({
+        settings = optional(string, "read")
+        teams    = optional(string, "none")
+      })
+      workspace_access = object({
+        runs           = optional(string, "read")
+        sentinel_mocks = optional(string, "none")
+        state_versions = optional(string, "none")
+        variables      = optional(string, "none")
+        create         = optional(bool, false)
+        locking        = optional(bool, false)
+        delete         = optional(bool, false)
+        move           = optional(bool, false)
+        run_tasks      = optional(bool, false)
+
+      })
+    })
+  )
+}
